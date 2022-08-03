57190a24b3d44f2e9c232008d2281fd56b374333
wjalong <376183066@qq.com>
2022-08-03 12:45:57 -0400

    Initial commit

---
+++ src/main/java/com/posbee/api/PosbeeServerApplication.java
@@ -0,0 +1,13 @@
+package com.posbee.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PosbeeServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PosbeeServerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/posbee/api/PosbeeServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.posbee.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PosbeeServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
